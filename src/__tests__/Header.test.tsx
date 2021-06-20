caa073a9881ddd7730b1e4ff60d21507c0f4706a
SergeyYahdzhyiev <mr.blooods@gmail.com>
2021-06-20 03:26:30 +0300

    developed header

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { Header } from '../components/Header';
 
 describe('Header', () => {
   test('should render component', () => {
-    const component = render(<Header />);
+    const component = render(
+      <Router>
+        <Header />
+      </Router>,
+    );
     expect(component.getAllByRole('logo')).toBeTruthy();
     expect(component.getAllByRole('nav-link')).toBeTruthy();
     expect(component).toMatchSnapshot();
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const TopBar = styled.header`
@@ -14,7 +15,47 @@ const TopBar = styled.header`
   border-bottom: 2px solid var(--clr-text-secondary);
   box-shadow: var(--box-shadow-primary);
 `;
+const LogoText = styled.h2`
+  font-size: max(32px, 3vw);
+  margin: 0;
+  font-weight: 500;
+`;
+const Colored = styled.span`
+  color: var(--clr-text-highlight);
+  font-weight: 700;
+`;
+const Menu = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+const NLink = styled(NavLink)`
+  font-size: max(16px, 1.5vw);
+  margin: 0 max(10px, 2vw);
+  text-decoration: none;
+  color: var(--clr-text-secondary);
+  transition: color 0.3s ease;
+
+  &:hover,
+  &:focus,
+  &.active {
+    color: var(--clr-text-highlight);
+  }
+`;
 
 export const Header: React.FC = () => {
-  return <TopBar></TopBar>;
+  return (
+    <TopBar>
+      <LogoText role="logo">
+        <Colored>J</Colored>ust<Colored>D</Colored>o<Colored>I</Colored>t
+      </LogoText>
+      <Menu>
+        <NLink to="/login" role="nav-link" activeClassName="active">
+          Login
+        </NLink>
+        <NLink to="/register" role="nav-link">
+          Register
+        </NLink>
+      </Menu>
+    </TopBar>
+  );
 };
