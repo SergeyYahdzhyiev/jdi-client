cf57ca3a0b11888f7e1f430d4d66414c465e0915
SergeyYahdzhyiev <mr.blooods@gmail.com>
2021-07-20 18:43:29 +0300

    realized routes change on auth, added mock main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React, { useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useCallback } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header, Footer, LoginForm, RegisterForm, Alert, Banner } from './components';
+import { Main } from './components/Main';
+import { useStores } from './hooks/stores.hook';
 
-export const App: React.FC = () => {
-  useEffect(() => {
-    console.log(document.cookie);
-  }, []);
+export const App: React.FC = observer(() => {
+  const { userStore } = useStores();
+
+  const getRoutes = useCallback(() => {
+    if (userStore.token) {
+      return <Route path="/" component={Main} />;
+    }
+    return (
+      <>
+        <Route exact path="/" component={Banner} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/register" component={RegisterForm} />
+      </>
+    );
+  }, [userStore.token]);
   return (
     <Router>
       <Header />
       <main>
-        <Switch>
-          <Route exact path="/" component={Banner} />
-          <Route exact path="/login" component={LoginForm} />
-          <Route exact path="/register" component={RegisterForm} />
-        </Switch>
+        <Switch>{getRoutes()}</Switch>
         <Alert />
       </main>
       <Footer />
     </Router>
   );
-};
+});
---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Main: React.FC = () => {
+  return (
+    <div>
+      <h2>Index</h2>
+    </div>
+  );
+};
