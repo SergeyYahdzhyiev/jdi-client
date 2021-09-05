c16d39029b7e608517ea7c78be685ca1f584cdb5
SergeyYahdzhyiev <mr.blooods@gmail.com>
2021-09-05 16:56:30 +0300

    developed main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import { TodoList } from './TodoList';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: var(--bg-secondary);
+
+  min-height: 80vh;
+`;
+const Wrapper = styled.div`
+  display: flex;
+
+  width: 100%;
+  max-width: 1024px;
+
+  padding: 2rem 1rem;
+`;
 
 export const Main: React.FC = () => {
   return (
-    <div>
-      <h2>Index</h2>
-    </div>
+    <Container>
+      <Wrapper>
+        <TodoList />
+      </Wrapper>
+    </Container>
   );
 };
